3ad3d1f316bd3d48c1618745ce056f4164ffbcf8
Ben McLemore <bmclemore@uchicago.edu>
2023-12-03 13:59:12 -0600

    saving general progress

--- LinearCombinations.v
+++ LinearCombinations.v
@@ -2,16 +2,28 @@ Set Warnings "-notation-overridden,-parsing,-deprecated-hint-without-locality".
 From LF Require Export VectorFields.
 Set Default Goal Selector "!".
 Require Import Vector.
-(* In order to use Full_set in span *)
-Require Import Ensembles.
+Require Import Logic.FunctionalExtensionality.
+Require Import Logic.PropExtensionality.
 
 Module Type LinearCombinations.
 Axiom v_th : vector_space_theory v_add s_v_mult 0v eq.
 (* 
 	We'll use the Coq standard library's definition of vectors, which is
 	"a list of size n whose elements belong to a set A."
-	We need to prove that a set of vectors satisfies the vector space axioms
-	under some defined operations, which we will define here:
+	NOTE: These vectors DO NOT represent vectors as we've defined them;
+	instead, these standard-library vectors represent finite sets of
+	our vectors, which is necessary to prove theorems about bases, linear
+	combinations, etc.
+
+	Our goal is to prove that given three vector spaces V1, V2, V3, if
+	V3 has degree m as a V2 vector space and V2 has degree n as a V1 vector
+	space, then V3 has degree m*n as a V1 vector space.
+	This requires:
+	defining the degree of a vector space ('n' in t V n of
+	a generating set);
+	defining span and linear independence (required to be a basis);
+	constructing (?) the basis for V3:V1 thru V3:V2 & V2:V1
+
 *)
 
 Global Notation "[ ]" := (nil _) (format "[ ]").
@@ -22,32 +34,71 @@ Global Notation "h :: t" := (cons _ h _ t) (at level 60, right associativity).
 	map2 takes fn g, vec s1 s2 ..., vec v1 v2 ..., and returns 
 	vec (g s1 v1) (g s2 v2) ... (g sn vn)
 *)
-Definition linear_com : forall {n : nat} (s : t Sclrs n) (v : t V n), t V n :=
-	fix linear_com_fix {n} (s : t Sclrs n) (v : t V n) : t V n := 
-		(map2 s_v_mult) s v.
+Definition linear_com : forall {n : nat} (s : t Sclrs n) (v : t V n), V :=
+	fix linear_com_fix {n} (s : t Sclrs n) (v : t V n) : V := 
+		fold_left v_add 0v ((map2 s_v_mult) s v).
+
+Hint Unfold linear_com : datatypes.
+
+(* Useless test definition *)
+Definition first_element {n} (v : t V n) : V :=
+	match v with
+	| [] => 0v
+	| fst :: tail => fst
+	end.
 
 (* The zero vector *)
 Global Notation "[0v]" := (nil V).
 
 (* 
-	Now we'd like to show that these operations form a vector space:
-	...
+	Now we'd like to show that given a specific n, all vectors t Sclrs n
+	form a vector space:
+	Therefore we need addition, scalar multiplication, a zero vector, and an equality operator.
+	We then submit these to a vector_space_theory record.
 *)
 
+Check vector_space_theory.
+
+(* Definition vector_space_theory *)
+
 (* TODO *)
-(* Define basis *)
+(* Define subset/subspace *)
 
 (*
 	A linear combination of vectors is a sum
 	(a1v1 + a2v2 + a3v3 + ...) where a_i are scalars.
 	A set of vectors is linearly independent if ONLY the trivial 
 	combination is equal to 0.
+	Here the proposition is stating that for all linear combinations
 *)
 Definition lin_indep {n : nat} (vs : t V n) : Prop :=
   forall (coeffs : t Sclrs n),
-    fold_left v_add 0v (linear_com coeffs vs) = 0v ->
+    (linear_com coeffs vs) = 0v ->
 	(* Forall is a vector proposition that a proposition holds over all elements *)
     Forall (fun y => y = 0) coeffs.
+
+Check lin_indep.
+Check linear_com.
+Hint Unfold lin_indep : datatypes.
+
+(* 
+	Helper lemma mirroring (LADW 1.6) from VectorFields.v:
+	If a set has no elements, it must be the empty set
+*)
+Lemma empty_set_unique {A : Type} : forall (set : t A 0), set = nil A.
+Proof. 
+	apply case0 (* From the inductive type of t *). 
+	reflexivity. 
+Qed.
+
+(* The empty set is linearly independent *)
+Theorem empty_set_is_linearly_independent : lin_indep (@nil V).
+Proof.
+	unfold lin_indep; intros. 
+	specialize (empty_set_unique coeffs); intros.
+	subst.
+	econstructor.
+Qed.
 
 (* 
 	Create spanning set:
@@ -55,20 +106,54 @@ Definition lin_indep {n : nat} (vs : t V n) : Prop :=
 	fold_left in the standard library works by:
 	f b x1 ... xn = f ... (f (f b x1) x2) ... xn
 
-	Proposition of in_span: a spans b if some coefficients
-	exist to create b as linear combination of vecs in a
+	Proposition of in_span: vecs spans v if some coefficients
+	exist to create v as linear combination
 *)
-Definition in_span {n : nat} (a : t V n) (b : V) : Prop :=
-	exists alpha, b = fold_left v_add 0v (linear_com alpha a).
+Definition in_span {n : nat} (vecs : t V n) (v : V) : Prop :=
+	exists coeffs, v = (linear_com coeffs vecs).
+
+(* Being in the span of the empty set is equivalent to being the zero vector *)
+Theorem span_empty_zero : in_span (@nil V) = fun (x : V) => x = 0v.
+Proof.
+	apply (functional_extensionality (in_span (@nil V)) (fun x => x = 0v)).
+	intros; unfold in_span.
+	apply propositional_extensionality; split; intros.
+	Admitted.
+
+Hint Unfold in_span : datatypes.
 
 (*
 	Proposition of generates: the set of vectors in the span
 	of some set of vectors is equal to the entire vector space.
-	This is equivalent to vs being a basis for V.
 *)
 Definition generates {n : nat} (vs : t V n) : Prop
-  :=
-    in_span vs = Full_set V.
+  := forall (v : V), in_span vs v.
+
+Hint Unfold generates : datatypes.
+
+(* A basis must generate a space AND be linearly independent *)
+Definition basis {n : nat} (vs : t V n) : Prop
+	:= generates vs /\ lin_indep vs.
+
+Hint Unfold generates : datatypes.
+
+(* (LADW 2.2) A basis need not contain 0v *)
+Theorem basis_without_zero : forall (n : nat), exists (vs : t V n),
+	(* 	There exists a set vs which generates but for all vectors in vs,
+		no vector is equal to the zero vector *)
+	generates vs /\ ~ Exists (eq 0v) vs.
+Proof.
+	(* How to prove non-constructively? *)
+	(* Might be a poorly described theorem *)
+	Admitted.
+
+(* 
+	Lemma 1 For tower theorem:
+	If V3 is a V2 vector space and V2 is a V1 vector space, V3 is a V1
+	vector space
+*)
+(* Theorem vector_field_extension : forall (n : nat), *)
+
 
 (* 
 	"shiftin" in the standard library adds an element to the end
@@ -77,9 +162,10 @@ Definition generates {n : nat} (vs : t V n) : Prop
 Infix "++" := shiftin.
 (* (LADW 2.5) A vector not in the span is linearly independent *)
 Theorem not_span_lin_indep: forall (n : nat) (a : t V n) (b : V),
-	~ in_span a b <-> lin_indep (b ++ a).
+	~ in_span a b -> lin_indep (b ++ a).
 Proof. 
-	intros; split; intros.
+	(* Informally: ... *)
+	unfold not. intros. 
 	- Admitted.
 
 End LinearCombinations.
--- README.md
+++ README.md
@@ -6,12 +6,11 @@ My proof of this will involve the fact a field extension K3/K1 can be thought of
 
 Therefore, I've opted to use Mathematical Components's framework for field extensions and build my own library (most of it, anyways) for vector spaces, then bridge the gap between the two in order to prove the Tower Theorem.
 
-Here's a basic outline of the theorem's we'll want in order to prove the Tower Theorem:
+Here's a basic outline of the theorems we'll want in order to prove the Tower Theorem:
 
 1. If L/K is a field extension, then L is also a K-vector space
-2. The degree [L : K] is equal to the cardinality of the basis of L as a K-vector space
-3. If K3 is a K2-VS, and K2 is a K1-VS, then K3 is a K1-VS whose basis has cardinality equal to the product of the bases of K3 as a K2-VS and K2 as a K1-VS
+2. If K3 is a K2-VS, and K2 is a K1-VS, then K3 is a K1-VS whose basis has cardinality equal to the product of the bases of K3 as a K2-VS and K2 as a K1-VS
 
-This project owes a great deal of its initial framework (a majority of the VectorFields file) to the work done by Quinn Dougherty on his YouTube series "Linear Algebra Done Right In Coq". I would not have been able to formalize Vector Spaces without this series.
+This project owes a great deal of its initial framework (almost all of the VectorAxioms module) to the work done by Quinn Dougherty on his YouTube series "Linear Algebra Done Right In Coq". I would not have been able to formalize Vector Spaces without this series.
 
 For exercises to prove as the library is built up, I am choosing to use Linear Algebra Done Wrong, the textbook we use in our Abstract Linear Algebra course at UChicago.
